27e273afca4428bd8dae6a14871b2c228b176bb7
Himanshu <himanshu.chhetri@addteq.com>
2014-09-16 12:51:31 -0400

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd /data 
-wget -q http://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-5.5.6-x64.bin
-chmod +x atlassian-confluence-5.5.6-x64.bin
-./atlassian-confluence-5.5.6-x64.bin <<<"o
+wget -q http://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-5.6.1-x64.bin
+chmod +x atlassian-confluence-5.6.1-x64.bin
+./atlassian-confluence-5.6.1-x64.bin <<<"o
 1
 "
