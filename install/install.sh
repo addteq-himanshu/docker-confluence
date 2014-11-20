91c33ff524f44b6eb8396ab7789fff99f2e5bf6b
Himanshu <himanshu.chhetri@addteq.com>
2014-11-19 22:37:41 -0500

    Updated install.sh for Confluence 5.6.4

--- install/install.sh
+++ install/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd /data 
-wget -q http://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-5.6.3-x64.bin
-chmod +x atlassian-confluence-5.6.3-x64.bin
-./atlassian-confluence-5.6.3-x64.bin <<<"o
+wget -q http://www.atlassian.com/software/confluence/downloads/binary/atlassian-confluence-5.6.4-x64.bin
+chmod +x atlassian-confluence-5.6.4-x64.bin
+./atlassian-confluence-5.6.4-x64.bin <<<"o
 1
 "
